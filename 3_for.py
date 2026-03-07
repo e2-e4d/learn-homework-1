d4dfe1cb68122f294d12a0eca16f81f14f8d0d76
Yura <akhmetshin.yura@gmail.com>
2026-03-08 02:24:49 +0300

    commit

--- 3_for.py
+++ 3_for.py
@@ -16,13 +16,38 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 def get_total_sales_for_each_product(sales_stats):
-    
+    for item in sales_stats:
+        total = 0
+        for sold in item['items_sold']:
+            total = total + sold          
+        print(f"Cуммарное количество продаж  {item['product']}: {total}")
+    print('\n')
 def get_average_sales_for_each_product(sales_stats):
-    
+    for item in sales_stats:
+        total = 0
+        items = 0
+        for sold in item['items_sold']:
+            total = total + sold
+            items +=1
+        average_sales = total / items
+        print(f"Cреднее количество продаж {item['product']}: {average_sales}")
+    print('\n')
 def get_total_sales_for_all_products(sales_stats):
-    
+    total = 0
+    for item in sales_stats:
+        for sold in item['items_sold']:
+            total = total + sold    
+    print(f"Cуммарное количество продаж всех товаров: {total}")
+    print('\n')
 def get_average_sales_for_all_products(sales_stats):
-
+    total = 0
+    items = 0
+    for item in sales_stats:
+        for sold in item['items_sold']:
+            total = total + sold
+            items +=1
+        average_sales = total / items       
+    print(f"Cреднее количество продаж всех товаров: {average_sales}")
     
 def main():
     """
@@ -34,11 +59,12 @@ def main():
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
-    print('Cуммарное количество продаж для каждого товара: \n', get_total_sales_for_each_product(sales_stats))
-    print('Cреднее количество продаж для каждого товара: \n', get_average_sales_for_each_product(sales_stats))
-    print('Cуммарное количество продаж всех товаров: \n', get_total_sales_for_all_products(sales_stats))
-    print('Cреднее количество продаж всех товаров: \n', get_average_sales_for_all_products(sales_stats))
+    get_total_sales_for_each_product(sales_stats)
+    get_average_sales_for_each_product(sales_stats)
+    get_total_sales_for_all_products(sales_stats)
+    get_average_sales_for_all_products(sales_stats)
 
     
 if __name__ == "__main__":
     main()
+
