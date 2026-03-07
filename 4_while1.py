583aaa0a3fe9002186e2486ff3911b90b60b6472
Yura <akhmetshin.yura@gmail.com>
2026-03-08 02:39:16 +0300

    commit

--- 4_while1.py
+++ 4_while1.py
@@ -9,12 +9,14 @@
    
 """
 
-
 def hello_user():
     """
     Замените pass на ваш код
     """
-    pass
+    while True:
+        answer = input('Как дела? \n')
+        if answer.lower() == 'хорошо':
+            break     
 
     
 if __name__ == "__main__":
